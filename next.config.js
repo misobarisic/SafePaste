d0c967290b865eab2a5b6bbf3674e8524fb6674c
misobarisic <miki.barisic5@gmail.com>
2021-07-15 14:16:57 +0200

    next.config.js update

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ module.exports = {
                 permanent: false,
             },
             {
-                source: "/md",
+                source: "//md",
                 destination: `/`,
                 permanent: false,
             },
